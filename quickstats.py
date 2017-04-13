2dcca92bce5c6cb5c7b3f9cc757a7916567fdd20
shaharf <shaharf@outbrain.com>
2017-04-13 12:49:40 +0300

    some changes to the ease the uage of the script
    1. Added a warning  that script will not run with python 2.
    
    2. added coloring(red when an interface passed the threshold).
    
    3. threshold moved to mbit instead of bytes.

--- quickstats.py
+++ quickstats.py
@@ -1,23 +1,27 @@
-#!/usr/bin/python3
+#!/usr/bin/env python
+
+from __future__ import print_function
+import sys
+if sys.version_info.major != 3:
+    print ("need python 3 for this script")
+    sys.exit(1)
 
 from datetime import datetime
 import re, time, argparse
 
 from argparse import ArgumentParser
-
 ap = ArgumentParser()
 ap.add_argument('--threshold', metavar='RATE', type=float, default=0.,
                 dest='threshold',
-                help='Rate (in MB/s) below which output is supressed')
+                help='Rate (in Mbit/s) below which output is supressed')
 ap.add_argument('--interval', metavar='INTERVAL', type=float, default=1000.,
                 dest='interval',
                 help='Sampling interval (in milliseconds)')
 
 args = ap.parse_args()
 
-threshold = args.threshold
+threshold = (args.threshold * 1024 * 1024 / 8)
 interval = args.interval / 1000.
-
 line_re = re.compile(r'\s*(.*):\s*(\d+)(?:\s+\d+){7}\s+(\d+)(?:\s+\d+){7}\s*')
 
 then = None
@@ -38,12 +42,15 @@ while True:
         rates[key] = (rate(0), rate(1))
     if any([rate[0] >= threshold or rate[1] >= threshold
             for rate in rates.values()]):
-        print('{} UTC'.format(now), end='', flush=False)
+        print('{} UTC'.format(now), end='')
         for key in sorted(rates.keys()):
-            rx, tx = rates[key]
-            print(' {:8} {:10} {:10}'.format(key, rx, tx),
-                  end='', flush=False)
-        print('', flush=True)
+#          if key.startswith('eth'):
+          rx, tx = rates[key]
+          if rx >= threshold or tx >= threshold:
+            print(' || \x1b[0;30;41m {:4} {:4}m(rx) {:4}m(tx) \x1b[0m '.format(key, round(rx/1024/1024*8), round(tx/1024/1024*8)), end='')
+          else:
+            print(' || \x1b[6;30;42m {:4} {:4}m(rx) {:4}m(tx) \x1b[0m '.format(key, round(rx/1024/1024*8), round(tx/1024/1024*8)), end='')
+        print('')
     old_stat = new_stat
     then = now
     time.sleep(interval)
